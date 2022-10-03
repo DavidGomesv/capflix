29d08dd0de3055afa90ef0c0f5efaacb7f49e78d
David Gomes <davidgomestej@gmail.com>
2022-10-03 08:46:39 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
+import { ListarFilmesComponent } from './listar-filmes/listar-filmes.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CadastroFilmesComponent,
+    ListarFilmesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/listar-filmes/listar-filmes.component.ts
@@ -0,0 +1,14 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-filmes',
+  templateUrl: './listar-filmes.component.html',
+  styleUrls: ['./listar-filmes.component.css']
+})
+export class ListarFilmesComponent{
+
+
+
+
+}
